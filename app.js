28caa0e76fabb9d36d3bb0a71661b22438fed118
iqbalalayubbi <iqbal.is415@gmail.com>
2023-01-15 23:00:02 +0700

    disconect from db

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
-const mongoose = require('mongoose');
-const url = `mongodb+srv://iqbal:database123@cluster0.qofpz.mongodb.net/app?retryWrites=true&w=majority`;
+// const mongoose = require('mongoose');
+// const url = `mongodb+srv://iqbal:database123@cluster0.qofpz.mongodb.net/app?retryWrites=true&w=majority`;
 const express = require('express');
 const app = express();
-var util= require('util');
-var encoder = new util.TextEncoder('utf-8');
 require('dotenv').config()
 const port = process.env.PORT
 
 app.use(express.json());
 
-mongoose.set("strictQuery", false);
-const connectionParams = {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-}
+// mongoose.set("strictQuery", false);
+// const connectionParams = {
+//     useNewUrlParser : true,
+//     useUnifiedTopology : true
+// }
 
-mongoose.connect(url,connectionParams)
-    .then(() => {
-        console.log('successfully connect into database');
-    })
-    .catch(err => console.log(err))
+// mongoose.connect(url,connectionParams)
+//     .then(() => {
+//         console.log('successfully connect into database');
+//     })
+//     .catch(err => console.log(err))
     
-const User = mongoose.model('user',{
-    username: String,
-    password: String,
-    secret: String
-});
+// const User = mongoose.model('user',{
+//     username: String,
+//     password: String,
+//     secret: String
+// });
 
 app.get('/',(req,res) => {
-    // res.send('hello world')
-    User.find().then(result => res.json(result))
+    res.send('hello world')
+    // User.find().then(result => res.json(result))
 })
 
 app.listen(port,() => console.log('server is running'))
